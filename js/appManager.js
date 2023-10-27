7e4227b56c1c819c98ae6760e71f43cae17728cc
Hek-tor <EmilioCmDz@gmail.com>
2023-10-26 21:19:55 -0600

    Add new styles

--- js/appManager.js
+++ js/appManager.js
@@ -7,6 +7,16 @@
 
 export class AppManager {
     constructor() {
+        this.mainContainer = document.createElement('div');
+        this.mainContainer.className = 'mainContainer';
+        document.body.appendChild(this.mainContainer);
 
+        this.navViewController = document.createElement('div');
+        this.navViewController.className = 'navViewController';
+        this.mainContainer.appendChild(this.navViewController);
+
+        this.controllerContainer = document.createElement('div');
+        this.controllerContainer.className = 'controllerContainer';
+        this.mainContainer.appendChild(this.controllerContainer);
     }
 }
