cc0581701711c920e6315a85ad80ee86b2c8f0dd
Hek-tor <EmilioCmDz@gmail.com>
2023-10-26 18:42:12 -0600

    Añadir archivos base al proyecto

---
+++ js/appManager.js
@@ -0,0 +1,6 @@
+/**
+* @name appManager
+* @file Add a small description for this file.
+* @author Hector Emilio hectoremdz@gmail.com
+* @version 1.0.0
+*/
---
+++ js/main.js
@@ -0,0 +1,6 @@
+/**
+* @name main
+* @file Add a small description for this file.
+* @author Hector Emilio hectoremdz@gmail.com
+* @version 1.0.0
+*/
