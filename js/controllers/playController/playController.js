491c1d4516fd7a90e38ba561853b868863e12f99
Hek-tor <EmilioCmDz@gmail.com>
2023-11-11 21:35:10 -0600

    Add game features.

--- js/controllers/playController/playController.js
+++ js/controllers/playController/playController.js
@@ -9,10 +9,58 @@ export class PlayController extends Controller {
         this.view = new PlayView(this, parent);
         this.view.updateHUD(0, 0);
         this.service.getCards();
+
+        this.view.container.addEventListener('onCardSelected', this.onCardSelected.bind(this));
+
+        this.cardView1 = null;
+        this.cardView2 = null;
+        this.showingTimer = null;
     }
 
     receiveCards(cards) {
         this.cards = cards;
         this.view.showCards(this.cards);
+    }
+
+    onCardSelected(event) {
+        if (this.cardView1 !== null && this.cardView2 !== null) return;
+
+        let cardView = event.detail.cardView;
+
+        if (this.cardView1 === null) {
+            this.cardView1 = cardView;
+            this.cardView1.showIcon();
+        } else if (this.cardView2 === null) {
+            this.cardView2 = cardView;
+            this.cardView2.showIcon();
+        }
+
+        if (this.cardView1 !== null && this.cardView2 !== null) {
+            this.checkCardViews();
+        }
+    }
+
+    checkCardViews() {
+        if (this.cardView1.card.id === this.cardView2.card.id) {
+            this.cardView1.discovered();
+            this.cardView2.discovered();
+            this.clearCardsViews();
+        } else {
+            this.showingTimer = window.setTimeout
+                (this.hideCardsWhenNotEquals.bind(this), 1000)
+        }
+    }
+
+    hideCardsWhenNotEquals() {
+        window.clearTimeout(this.showingTimer);
+        this.showingTimer = null;
+        this.cardView1.hideIcon();
+        this.cardView2.hideIcon();
+        this.clearCardsViews();
+    }
+
+    clearCardsViews() {
+        this.cardView1 = null;
+        this.cardView2 = null;
     }
 }
--- js/views/cardsView/cardView.js
+++ js/views/cardsView/cardView.js
@@ -5,6 +5,30 @@ export class CardView extends View {
         super(parent);
         this.card = card;
         this.container.className = 'cardView';
+        this.container.onclick = this.onCardSelected.bind(this);
+    }
+
+    onCardSelected() {
+        const onCardSelectedEvent = new CustomEvent("onCardSelected", {
+            bubbles: true,
+            detail: { cardView: this },
+        });
+        this.container.dispatchEvent(onCardSelectedEvent);
+    }
+
+    showIcon() {
+        this.container.classList.add('cardView_show');
         this.container.innerHTML = this.card.icon;
+    }
+
+    discovered() {
+        this.container.classList.remove('cardView_show');
+        this.container.classList.add('cardView_discovered');
+    }
+
+    hideIcon() {
+        this.container.innerHTML = '';
+        this.container.classList.remove('cardView_show');
+        this.container.classList.remove('cardView_discovered');
     }
 }
