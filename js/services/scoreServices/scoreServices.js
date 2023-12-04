7ea7061df55124973cf69358ac71738d91def9e6
Hek-tor <EmilioCmDz@gmail.com>
2023-12-04 16:48:02 -0600

    Add new request url

--- js/services/scoreServices/scoreServices.js
+++ js/services/scoreServices/scoreServices.js
@@ -13,7 +13,7 @@ export class ScoresServices extends Service {
         request.open('GET', url);
         request.onload = () => {
             if (request.status >= 200 && request.status < 400) {
-                let dataScore = JSON.parse(request.response);
+                let dataScore = request.response;
             } else {
                 console.warn('Error en la solicitud. Estado:', request.status);
             }
